da305f4905643ca12eb32aaccb0377c32b2673fb
jeff kang <jeffyjkang@gmail.com>
2022-11-23 16:26:39 -0600

    crud operations

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .models import Notes
-
+from .forms import NotesForm
 class NotesListView(ListView):
   model = Notes
   context_object_name = 'notes'
@@ -13,13 +14,29 @@ class NotesDetailView(DetailView):
   model = Notes
   context_object_name = 'note'
 
+class NotesCreateView(CreateView):
+  model = Notes
+  # fields = ['title', 'text']
+  form_class = NotesForm
+  success_url = '/smart/notes'
+
+class NotesUpdateView(UpdateView):
+  model = Notes
+  form_class = NotesForm
+  success_url = '/smart/notes'
+
+class NotesDeleteView(DeleteView):
+  model = Notes
+  success_url = '/smart/notes'
+  template_name: str = 'notes/notes_delete.html'
+
 # def list(request):
 #   all_notes = Notes.objects.all()
 #   return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
-def detail(request, pk):
-  try:
-    note = Notes.objects.get(pk=pk)
-  except Notes.DoesNotExist:
-    raise Http404('Note does not exist')
-  return render(request, 'notes/notes_detail.html', {'note': note})
+# def detail(request, pk):
+#   try:
+#     note = Notes.objects.get(pk=pk)
+#   except Notes.DoesNotExist:
+#     raise Http404('Note does not exist')
+#   return render(request, 'notes/notes_detail.html', {'note': note})
