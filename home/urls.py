7cc2a0c0b2d69bf2e8abeeb3a20fd80732719eb5
jeff kang <jeffyjkang@gmail.com>
2022-11-24 00:46:03 -0600

    login and logout

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
   # path('home', views.home),
-  path('home', views.HomeView.as_view()),
+  # path('home', views.HomeView.as_view(), name='home'),
+  path('', views.HomeView.as_view(), name='home'),
   # path('authorized', views.authorized),
-  path('authorized', views.AuthorizedView.as_view()),
+  # path('authorized', views.AuthorizedView.as_view()),
+  path('login', views.LoginInterfaceView.as_view(), name='login'),
+  path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+  path('signup', views.SignupView.as_view(), name='signup'),
 ]
