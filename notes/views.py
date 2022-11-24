5db5654b908278c55f507f657b80ad19e204ad79
jeff kang <jeffyjkang@gmail.com>
2022-11-23 23:53:22 -0600

    user relationship

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from django.http import Http404
+# from django.http import Http404
+from django.http.response import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Notes
 from .forms import NotesForm
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
   model = Notes
   context_object_name = 'notes'
   template_name = 'notes/notes_list.html'
+  login_url = '/admin'
+
+  def get_queryset(self):
+    return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
   model = Notes
@@ -19,6 +25,12 @@ class NotesCreateView(CreateView):
   # fields = ['title', 'text']
   form_class = NotesForm
   success_url = '/smart/notes'
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.user = self.request.user
+    self.object.save()
+    return HttpResponseRedirect(self.get_success_url())
 
 class NotesUpdateView(UpdateView):
   model = Notes
